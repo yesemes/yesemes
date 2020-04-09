c4ff51d355a1eb876e247b2270bda24b196f8a79
sayyed <yesemes@gmail.com>
2020-04-09 13:38:28 -0500

    demo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ReactPlayer from 'react-player';
+function App() {
+  return (
+    <div className="App">
+      
+      <ReactPlayer url='https://www.youtube.com/watch?v=TlB_eWDSMt4'/>
+    </div>
+  );
+}
+
+export default App;
