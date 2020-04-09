1c44c4a72bcd2340c300d72a4d2301a322a6b55d
sayyed <yesemes@gmail.com>
2020-04-09 14:13:38 -0500

    Updates

---
+++ precache-manifest.152679ac0221ed5baa14a2cde9aac049.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3173f588b047ad96ebe8b18777d081eb",
+    "url": "/yesemes.github.io/demo/index.html"
+  },
+  {
+    "revision": "22fafd7860488135cb63",
+    "url": "/yesemes.github.io/demo/static/css/main.b7f521ac.chunk.css"
+  },
+  {
+    "revision": "72ffa950ae2a91d1bfdc",
+    "url": "/yesemes.github.io/demo/static/js/2.20aadfb9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yesemes.github.io/demo/static/js/2.20aadfb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22fafd7860488135cb63",
+    "url": "/yesemes.github.io/demo/static/js/main.f33ac99a.chunk.js"
+  },
+  {
+    "revision": "e0f4217f0c4585d5f9ba",
+    "url": "/yesemes.github.io/demo/static/js/runtime-main.96256570.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/yesemes.github.io/demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
